7f0a84c60fb84925242e51144f7279fd4c6e5b38
RamR3R <sendtoramsundar@gmail.com>
2023-02-25 09:07:54 +0530

    Search function added

--- scripts/index.js
+++ scripts/index.js
@@ -8,12 +8,31 @@ fetch(api)
     display(DataBase);
 })
 
-let search = document.getElementById("")
+let search = document.getElementById("search-btn");
+search.addEventListener("click",()=>{
+    let searchInput  = document.getElementById("search-input")
+    let api = `https://63f59a1b3f99f5855dc408c8.mockapi.io/Assets/Products/?search=${searchInput.value}`;
+    console.log(search)
+    fetch(api)
+    .then(result=>result.json())
+    .then(data=>{
+    console.log(data);
+    DataBase = data;
+    display(DataBase);
+    })
+})
 
 
 function display(data)
 {
     let body  = document.querySelector("#container");
+    body.innerHTML = null;
+    if(data.length == 0){
+        let h1 = document.createElement("h1");
+        h1.innerText = "No result Found :(";
+        body.append(h1);
+    }
+    else
     data.forEach(element => {
         let card = document.createElement("div");
         let img = document.createElement("img");
